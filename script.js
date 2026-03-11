799158b7130446b7839a0cb25084104b661b4312
Nalini Kant <104712103+NaliniKant1@users.noreply.github.com>
2026-03-11 23:29:15 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+window.addEventListener("load",function(){
+
+const track=document.querySelector(".scroll-track");
+
+if(track){
+
+let speed=1;
+
+function autoScroll(){
+
+track.scrollLeft+=speed;
+
+if(track.scrollLeft>=track.scrollWidth/2){
+track.scrollLeft=0;
+}
+
+}
+
+setInterval(autoScroll,20);
+
+}
+
+});
